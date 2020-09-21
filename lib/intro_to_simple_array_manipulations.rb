1fb6890c8217aa33913196342e43d689b71b8132
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-21 21:29:51 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+
+end
+
+def using_unshift(array, string)
+  
+end
+
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
